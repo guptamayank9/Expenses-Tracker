557d3939b4507a8f008d7306c779e2e05b5065c0
guptamayank9 <guptamayank242032@gmail.com>
2026-09-16 13:01:06 +0530

    add mongodb connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,44 +13,92 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+const expenseSchema = new mongoose.Schema(
+    {
+        title:{
+            type:String,
+            required: true,
+        },
+        amount:{
+            type:Number,
+            required:true,
+        },
+    },
+    {
+        timestamps:true
+    }
+);
+//Expense Model
+const Expense = mongoose.model("Expense", expenseSchema);
+
+
+
 //Home Route
 app.get('/',(req,res)=>{
     res.send("Expense Tracker Backend is running");
 });
 
-//Temporary Expenses data
-let expenses= [];
-
 //Get all expenses
-app.get('/api/expenses',(req,res)=>{
-    res.json(expenses);
+app.get('/api/expenses', async (req,res) => {
+    try{
+        const expenses = await Expense.find().sort({createdAt:-1});
+        res.json(expenses);
+
+    }catch(error){
+        res.status(500).json({
+            message:"Failed to Fetch expenses",
+        });
+    }
 });
+// //Temporary Expenses data
+// let expenses= [];
+
+// //Get all expenses
+// app.get('/api/expenses',(req,res)=>{
+//     res.json(expenses);
+// });
+
 
 //Add new Expenses
-app.post('/api/expenses', (req,res)=>{
-    const {title, amount } = req.body;
+app.post('/api/expenses', async (req,res)=>{
 
+    try {
+         const {title, amount } = req.body;
+  
     if(!title || !amount){
         return res.status(400).json({
         message:"Title and amount are required",
     });
     }
-     
-    const newExpense = {
-        id:Date.now(),
+    
+    const newExpense = await Expense.create({//Ye data MongoDB database mein permanently save karega.
         title:title,
-        amaount: Number(amount),
-    };
-    expenses.push(newExpense);
+        amount:Number(amount),
+    });
 
     res.status(201).json({
         message:"Expense added Successfully",
         expense:newExpense,
     });
-
+    } catch (error) {
+        res.status(500).json({
+            message:"Failed to add expense",
+        });
+    }
 });
 
-//SERVER START
-app.listen(PORT,()=>{
+
+//Connect MONgoDb and start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(()=>{
+    console.log("Mongodb connected Successfully");
+    
+    app.listen(PORT,()=>{
     console.log(`Server running at ${PORT}`);
-});
+    });
+  })
+  .catch((error)=>{
+    console.log("Mongodb connection Failed");
+    console.log(error.message);
+  });
